d616df5b0dd09a99721df5d48b23100d660f82d9
James Newman <jamesmnewman1@gmail.com>
2015-07-09 16:49:46 -0700

    3 tables up

--- dbc-overflow/db/migrate/20150709230044_create_questions.rb
+++ dbc-overflow/db/migrate/20150709230044_create_questions.rb
@@ -1,6 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+    	t.string :title
+    	t.string :content
+    	t.references :user, index: true, foreign_key: true
+    	t.integer :vote_count, default: 0
+
+    	t.timestamps
     end
   end
 end
--- dbc-overflow/db/migrate/20150709230052_create_answers.rb
+++ dbc-overflow/db/migrate/20150709230052_create_answers.rb
@@ -1,6 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+    	t.text :content
+    	t.references :user, index: true, foreign_key: true
+    	t.references :question, index: true, foreign_key: true
+    	t.integer :vote_count, default: 0
+
+    	t.timestamps
     end
   end
 end
