ee4b48381247f71083f53502766cbd8fd41d4771
James Newman <jamesmnewman1@gmail.com>
2015-07-09 19:16:34 -0700

    routes

--- dbc-overflow/app/models/user.rb
+++ dbc-overflow/app/models/user.rb
@@ -1,3 +1,4 @@
 class User
-
+	has_many :questions
+	has_many :answers
 end
--- dbc-overflow/config/routes.rb
+++ dbc-overflow/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
- 
+ resources :questions do 
+  member do 
+    post 'upvote'
+    post 'downvote'
+  end
+  resources :answers do 
+    member do 
+      post 'upvote'
+      post 'downvote'
+     end 
+   end
+ end
 end
