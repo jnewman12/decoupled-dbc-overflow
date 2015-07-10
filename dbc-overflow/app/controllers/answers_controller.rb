9235d419f8a44d42fe4fc0b07aeed121d8cf201f
James Newman <jamesmnewman1@gmail.com>
2015-07-09 19:56:22 -0700

    finished refactored CRUD. Moving onto setting up angular and will come back to user auth stuff

--- dbc-overflow/app/controllers/answers_controller.rb
+++ dbc-overflow/app/controllers/answers_controller.rb
@@ -1,46 +1,134 @@
+# class AnswersController < ApplicationController
+# 	before_action :set_answer, only: [:show, :update, :destroy]
+
+
+# 	def new 
+# 		@answer = Answer.new
+# 		respond_with @answer
+# 	end
+
+# 	def edit
+# 		@answer = Answer.find(params[:id])
+# 		respond_with @answer
+# 	end
+
+# 	def show
+# 		@answer = Answer.find(params[:id])
+# 		respond_with @answer
+# 	end
+
+# 	def create
+# 		@answer = Answer.create(answers_params)
+# 		if @answer.save
+# 			respond_with @answer
+# 		else
+# 			render 'new'
+# 		end
+# 	end
+
+# 	def update
+# 		@answer.update(answers_params)
+# 		respond_with @answer
+# 	end
+
+# 	def destroy
+# 	end
+
+# 	def upvote
+# 	end
+
+# 	private
+# 	def upvote_answer
+# 	end
+
+# 	def answers_params
+#      params.require(:answer).permit(:content, :)
+#     end
+
+# end
+
 class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :update, :destroy]
 
-	def new 
-		@answer = Answer.new
-		respond_with @answer
-	end
+  # GET /answers
+  # GET /answers.json
+  def index
+    @answers = Answer.all
 
-	def edit
-		@answer = Answer.find(params[:id])
-		respond_with @answer
-	end
+    render json: @answers
+  end
 
-	def show
-		@answer = Answer.find(params[:id])
-		respond_with @answer
-	end
+  # GET /answers/1
+  # GET /answers/1.json
+  def show
+    render json: @answer
+  end
 
-	def create
-		@answer = Answer.create(answers_params)
-		if @answer.save
-			respond_with @answer
-		else
-			render 'new'
-		end
-	end
+  def new
+    @answer = Answer.new
+  end
 
-	def update
-		@answer.update(answers_params)
-		respond_with @answer
-	end
+  # POST /answers
+  # POST /answers.json
+  def create
+    @answer = Answer.new(answer_params)
 
-	def destroy
-	end
+    if @answer.save
+      render json: @answer, status: :created, location: @answer
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
 
-	def upvote
-	end
+  def edit
+  end
 
-	private
-	def upvote_answer
-	end
+  # PATCH/PUT /answers/1
+  # PATCH/PUT /answers/1.json
+  def update
+    @answer = Answer.find(params[:id])
 
-	def answers_params
-     params.require(:answer).permit(:content)
+    if @answer.update(answer_params)
+      head :no_content
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /answers/1
+  # DELETE /answers/1.json
+  def destroy
+    @answer.destroy
+
+    head :no_content
+  end
+
+  def upvote
+  	upvote_answer
+  end
+
+  def downvote
+  	downvote_answer
+  end
+
+  private
+    def upvote_answer
+    	a = Answer.find(params[:id])
+    	a.vote_count += 1
+    	a.save
     end
 
+    def downvote_answer
+    	a = Answer.find(params[:id])
+    	a.vote_count -= 1
+    	a.save
+    end
+
+    def set_answer
+      @answer = Answer.find(params[:id])
+    end
+
+    def answer_params
+      params.require(:answer).permit(:content, :question_id)
+    end
 end
--- dbc-overflow/app/controllers/questions_controller.rb
+++ dbc-overflow/app/controllers/questions_controller.rb
@@ -1,53 +1,167 @@
+# class QuestionsController < ApplicationController
+
+# 	before_action :set_question, only: [:show, :update, :destroy]
+#     before_action :allow_cross_domain
+
+# 	# all redirects are taken out, because the controllers only job is to render json or if something doesnt save, render new form
+
+# 	def index
+# 		@questions = Question.all 
+# 		respond_with @questions
+# 	end
+
+# 	def new
+# 		@question = Question.new
+# 		respond_with @question
+# 	end
+
+# 	def show
+# 		@question = Question.find(params[:id])
+# 		respond_with @question
+# 	end
+
+# 	def create
+# 		@question = Question.new(question_params)
+# 		if @question.save
+# 			respond_with @question, status: :created, location: @question
+# 		else 
+# 			respond_with @question.errors, status: :unprocessable_entity
+# 		end
+# 	end
+
+# 	def edit
+# 	  @question = Question.find(params[:id])
+# 	  respond_with @question
+# 	end
+
+#    def update
+#    	@question = Question.find(params[:id])
+#    	if @question.update(question_params)
+#    		head :no_content
+#    	else
+#    		respond_with @question.errors, status: :unprocessable_entity
+#    	end
+#    end
+
+#    def destroy
+#    	@question.destroy
+#    end
+
+# 	def upvote
+# 	  upvote_question # this method might be done in angular instead of rails, not sure....
+# 	end
+
+# 	def downvote
+# 		downvote_question
+# 	end
+
+# 	private
+# 	def upvote_question
+#      q = Question.find(params[:id])
+#      q.vote_count += 1
+#      q.save
+#      #redirect_to '/'
+#     end
+
+#     def downvote_question
+#     	q = Question.find(params[:id])
+#     	q.vote_count -= 1
+#     	q.save	
+#     end
+
+#     def allow_cross_domain
+#       headers['Access-Control-Allow-Origin'] = '*'
+#       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+#       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+#     end
+# end
+
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :update, :destroy]
+  before_action :allow_cross_domain
 
-	before_action :allow_cross_origin
+  # GET /questions
+  # GET /questions.json
+  def index
+    @questions = Question.all
 
-	# all redirects are taken out, because the controllers only job is to render json or if something doesnt save, render new form
+    render json: @questions
+  end
 
-	def index
-		@questions = Question.all 
-		respond_with @questions
-	end
+  # GET /questions/1
+  # GET /questions/1.json
+  def show
+    render json: @question
+  end
 
-	def new
-		@question = Question.new
-		respond_with @question
-	end
+  def new
+    @question = Question.new
+    render json: @question
+  end
 
-	def show
-		@question = Question.find(params[:id])
-		respond_with @question
-	end
+  # POST /questions
+  # POST /questions.json
+  def create
+    @question = Question.new(question_params)
 
-	def create
-		@question = Question.new(question_params)
-		if @question.save
-			respond_with @question
-		else 
-			render 'new'
-		end
-	end
+    if @question.save
+      render json: @question, status: :created, location: @question
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
+  end
 
-	def edit
-	  @question = Question.find(params[:id])
-	  respond_with @question
-	end
+  def edit
 
-   def update
-    @question = Question.update(question_params)
-    respond_with @question
-   end
+  end
 
-	def upvote
-	  upvote_question # this method might be done in angular instead of rails, not sure....
-	end
+  # PATCH/PUT /questions/1
+  # PATCH/PUT /questions/1.json
+  def update
+    @question = Question.find(params[:id])
 
-	private
-	def upvote_question
+    if @question.update(question_params)
+      head :no_content
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /questions/1
+  # DELETE /questions/1.json
+  def destroy
+    @question.destroy
+
+    head :no_content
+  end
+
+  def upvote
+    upvote_question # this method might be done in angular instead of rails, not sure....
+  end
+
+  def downvote
+	downvote_question
+  end
+  private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :content)
+    end
+
+    def upvote_question
      q = Question.find(params[:id])
      q.vote_count += 1
      q.save
-     #redirect_to '/'
+    end
+
+    def downvote_question
+	 q = Question.find(params[:id])
+	 q.vote_count -= 1
+	 q.save	
     end
 
     def allow_cross_domain
@@ -55,5 +169,4 @@ class QuestionsController < ApplicationController
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
     end
-
 end
